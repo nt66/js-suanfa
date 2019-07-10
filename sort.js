7f41f861751234f384d00fd055f5aee6540707ed
hualingfeng <tc_hualingfeng@163.com>
2019-07-10 11:32:27 +0800

    快排

--- sort.js
+++ sort.js
@@ -40,5 +40,55 @@ function selectionSort(arr) {
   return arr;
 }
 
-console.log('result:',selectionSort(ARR))
+
+//快速排序
+/*方法说明：快速排序
+@param  array 待排序数组*/
+//方法一
+function quickSort(array, left, right) {
+  console.time('1.快速排序耗时');
+  if (Object.prototype.toString.call(array).slice(8, -1) === 'Array' && typeof left === 'number' && typeof right === 'number') {
+      if (left < right) {
+          var x = array[right], i = left - 1, temp;
+          for (var j = left; j <= right; j++) {
+              if (array[j] <= x) {
+                  i++;
+                  temp = array[i];
+                  array[i] = array[j];
+                  array[j] = temp;
+              }
+          }
+          quickSort(array, left, i - 1);
+          quickSort(array, i + 1, right);
+      }
+      console.timeEnd('1.快速排序耗时');
+      return array;
+  } else {
+      return 'array is not an Array or left or right is not a number!';
+  }
+}
+// console.log(quickSort(arr,0,arr.length-1));//[2, 3, 4, 5, 15, 19, 26, 27, 36, 38, 44, 46, 47, 48, 50]
+
+//方法二
+var quickSort2 = function (arr) {
+  // console.time('快速排序耗时');
+  if (arr.length <= 1) { return arr; }
+  var pivotIndex = Math.floor(arr.length / 2);
+  var pivot = arr.splice(pivotIndex, 1)[0];
+  var left = [];
+  var right = [];
+  for (var i = 0; i < arr.length; i++) {
+    if (arr[i] < pivot) {
+      left.push(arr[i]);
+    } else {
+      right.push(arr[i]);
+    }
+  }
+  // console.log(left.concat(pivot,right),pivot)
+  // console.timeEnd('快速排序耗时');
+  return quickSort2(left).concat([pivot], quickSort2(right));
+};
+
+
+// console.log('result:',quickSort2(ARR)) //[2, 3, 4, 5, 15, 19, 26, 27, 36, 38, 44, 46, 47, 48, 50]
  
