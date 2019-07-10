78b7baac7d4f83a5411f510777e7db2e679d8713
hualingfeng <tc_hualingfeng@163.com>
2019-07-10 17:38:40 +0800

    ~

--- counts.js
+++ counts.js
@@ -1,16 +1,16 @@
 /**
  * 统计
  */
+const LIST = [1, 2, 3, 3, 3, 3, 5, 4, 5, 6, 6, 7, 8, 333, 4444];
 
- const LIST = [ 1, 2, 3, 3, 3, 3, 5, 4, 5, 6, 6, 7, 8, 333, 4444 ];
- function getWordCnt(){
+function getWordCnt() {
   let Obj = {}
-  for(var item of LIST){
-    Obj[item] = (Obj[item]+1)||1
+  for (var item of LIST) {
+    Obj[item] = (Obj[item] + 1) || 1
   }
   return Obj;
- }
+}
 
- console.log(getWordCnt())
+console.log(getWordCnt(LIST))
 
 
