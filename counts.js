3e514ce9bbe785b89ffbea330ed0101cf9810330
hualingfeng <tc_hualingfeng@163.com>
2019-07-14 21:12:13 +0800

    ~

--- counts.js
+++ counts.js
@@ -11,6 +11,19 @@ function getWordCnt() {
   return Obj;
 }
 
-console.log(getWordCnt(LIST))
+// console.log(getWordCnt(LIST))
+
+
+// var s = new Set(['A', 'B', 'C']);
+var ary = [1,2,3,4,5,6,10];
+const itArr=ary[Symbol.iterator]();
+// for (var x of s) { // 遍历Set
+//   console.log(x);
+// }
+console.log(itArr.next());
+console.log(itArr.next());
+console.log(itArr.next());
+console.log(itArr.next());
+
 
 
