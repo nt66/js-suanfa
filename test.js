20ebd6a1707023f580f85f3d7ccb7ec254556516
hualingfeng <tc_hualingfeng@163.com>
2019-07-15 22:42:13 +0800

    test

---
+++ test.js
@@ -0,0 +1,22 @@
+/**
+ * generator
+ */
+// function* gen() { 
+//   yield 1;
+//   yield 2;
+//   yield 3;
+// }
+// let g = gen();
+// console.log(g.next());
+// console.log(g.next());
+// console.log(g.next());
+
+/**
+ * splice 增、删
+ */
+const ary = ['a','n','ui','23'];
+console.log(ary)
+ary.splice(1,0,'22','333');
+console.log(ary)
+
+
