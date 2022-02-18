5103641df671fb56d575b2b8fdbd563762fc308a
hualingfeng <tchualingfeng@gmail.com>
2022-02-18 14:52:06 +0800

    Update flatten.js

--- flatten.js
+++ flatten.js
@@ -6,19 +6,20 @@ var arr = [1, 2, 3, [3, 3, 3, [5, 4, 5, 6, 6, 7, 8]],[333, 4444]];
 // console.log(t.constructor == Array) 
 /* 判断是否是某某类型 t.constructor == Array、t instanceof Array、Object.prototype.toString.call(t) ==='[object Array]'、Array.isArray(t)*/
 
-// function flatten(ary){
-//   for(var t of ary){
-//     if(!Array.isArray(t)){
-//       newAry.push(t)
-//     }else{
-//       newAry.concat(flatten(t))
-//     }
-//   }
-//   return newAry
-// }
+// 方式一
+let newAry = []
+function flatten(ary){
+  for(var t of ary){
+    if(!Array.isArray(t)){
+      newAry.push(t)
+    }else{
+      newAry.concat(flatten(t))
+    }
+  }
+  return newAry
+}
 
-// console.log('result:', flatten(arr))
-
+// 方式二
 let newAry = []
 function flatten2(ary){
   for(var t of ary){
@@ -26,4 +27,8 @@ function flatten2(ary){
   }
   return newAry;
 }
-console.log('result:', flatten2(arr))
+
+
+// 方式三
+let newAry = arr.flat(Infinity);
+
